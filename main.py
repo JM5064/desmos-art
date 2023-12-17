7e3c64518cdcf8c9c86e4f3dce2fce98005dd918
JM5064 <110075590+JM5064@users.noreply.github.com>
2023-12-17 12:05:51 -0500

    Update main.py
    Smooth curves, fixed stray lines issue

--- main.py
+++ main.py
@@ -17,6 +17,11 @@ def process_image(image):
     return image
 
 
+def display_image(image):
+    cv.imshow("Awesome Face", image)
+    k = cv.waitKey(0)
+
+
 def get_points(image):
     width, height = image.shape
     frequency = 4
@@ -42,7 +47,7 @@ def get_points(image):
     return np.asarray(threes)
 
 
-def calculate_circle(coord_set):
+def calculate_circle_old(coord_set):
     a1, a2, a3 = coord_set[0][0], coord_set[1][0], coord_set[2][0]
     b1, b2, b3 = coord_set[0][1], coord_set[1][1], coord_set[2][1]
     c1, c2, c3 = 1, 1, 1
@@ -63,7 +68,7 @@ def calculate_circle(coord_set):
         return None
 
 
-def calculate_circle2(coord_set):
+def calculate_circle(coord_set):
     a1, a2, a3 = coord_set[0][0], coord_set[1][0], coord_set[2][0]
     b1, b2, b3 = coord_set[0][1], coord_set[1][1], coord_set[2][1]
 
@@ -108,6 +113,13 @@ def is_vertical(coord_set):
     return False
 
 
+def contains_duplicate(coord_set):
+    if np.array_equal(coord_set[0], coord_set[1]) or np.array_equal(coord_set[0], coord_set[2]) \
+            or np.array_equal(coord_set[1], coord_set[2]):
+        return True
+    return False
+
+
 def calculate_slope(coord_set):
     a1, a3 = coord_set[0][0], coord_set[2][0]
     b1, b3 = coord_set[0][1], coord_set[2][1]
@@ -129,61 +141,69 @@ def calculate_y_bounds(coord_set):
     return [minimum, maximum]
 
 
-def display_image(image):
-    cv.imshow("Awesome Face", image)
-    k = cv.waitKey(0)
-
-
-def get_contour_points(image):
-    width, height = image.shape
-    frequency = 3
+def get_contour_points(con):
+    frequency = 6
 
     count = 0
     coords = []
 
-    print(len(contours), "len contours")
-    print(len(contours[6]))
+    print(len(con), "len contours")
 
-    for i in range(len(contours)):
-        for j in range(len(contours[i])):
+    for i in range(len(con)):
+        for j in range(len(con[i])):
             if count == frequency:
+                # if len(coords) % 3 != 0 or len(con[i]) - j > 2 * (frequency + 1):
                 # print("(%d, %d)" % (contours[i][j][0][0], contours[i][j][0][1]))
-                coords.append([contours[i][j][0][0], contours[i][j][0][1]])
+                coords.append([con[i][j][0][0], con[i][j][0][1]])
                 count = 0
             else:
                 count += 1
+        coords.append([None, None])
+    print("len coords:", len(coords))
 
     threes = []
-    for i in range(0, len(coords), 2):
-        if len(coords) - i > 2:
+    i = 0
+    while i < len(coords) - 2:
+        if coords[i][0] is None or coords[i + 1][0] is None or coords[i + 2][0] is None:
+            i += 1
+        else:
             threes.append([coords[i], coords[i + 1], coords[i + 2]])
-
+            i += 2
+    print("len threes:", len(threes))
     return np.asarray(threes)
 
 
 img = process_image(img)
 
-contours, hierarchy = cv.findContours(img, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+contours, hierarchy = cv.findContours(img, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
 
 display_image(img)
 
-points = get_contour_points(img)
+points = get_contour_points(contours)
+num_equations = 0
 
 for p in points:
     x_bounds = calculate_x_bounds(p)
     y_bounds = calculate_y_bounds(p)
-    if is_collinear(p):
-        if is_vertical(p):
-            print("x = %d \\left\\{%d < y < %d\\right\\}"
-                  % (p[0][0], y_bounds[0], y_bounds[1]))
+    if not contains_duplicate(p):
+        if is_collinear(p):
+            if is_vertical(p):
+                print("x = %0.2f \\left\\{%0.2f < y < %0.2f\\right\\}"
+                      % (p[0][0], y_bounds[0], y_bounds[1]))
+                num_equations += 1
+            else:
+                slope = calculate_slope(p)
+                print("y - %0.2f = %0.2f(x - %0.2f) \\left\\{%0.2f < x < %0.2f\\right\\}" % (p[0][1], slope, p[0][0], x_bounds[0], x_bounds[1]))
+                num_equations += 1
         else:
-            slope = calculate_slope(p)
-            print("y - %d = %d(x - %d) \\left\\{%d < x < %d\\right\\}" % (p[0][1], slope, p[0][0], x_bounds[0], x_bounds[1]))
-    else:
-        circle = calculate_circle2(p)
-        if circle is not None:
-            print("(x - %d)^2 + (y - %d)^2 = %d \\left\\{%d < x < %d\\right\\} \\left\\{%d < y < %d\\right\\}"
-                  % (circle[0], circle[1], circle[2], x_bounds[0], x_bounds[1], y_bounds[0], y_bounds[1]))
+            circle = calculate_circle(p)
+            if circle is not None:
+                print("(x - %0.2f)^2 + (y - %0.2f)^2 = %0.2f \\left\\{%0.2f < x < %0.2f\\right\\} \\left\\{%0.2f < y "
+                      "< %0.2f\\right\\}"
+                      % (circle[0], circle[1], circle[2], x_bounds[0], x_bounds[1], y_bounds[0], y_bounds[1]))
+                num_equations += 1
+
+print("Number of equations:", num_equations)
 
 cv.drawContours(img, contours, -1, (0, 255, 0), 1)
 cv.imshow("contours", img)
