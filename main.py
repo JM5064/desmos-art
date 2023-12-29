a83b2c14f23d377f311a0527f9f6d49ab8fdc3fc
JM5064 <110075590+JM5064@users.noreply.github.com>
2023-12-29 16:05:34 -0500

    Added color

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import cv2 as cv
 import numpy as np
 
-url = "images/mcgill.jpeg"
+url = "images/messi.png"
 img = cv.imread(url)
+img = cv.rotate(img, cv.ROTATE_180)
+img = cv.flip(img, 1)
 
 if img is None:
     print("Image not found")
@@ -12,8 +14,8 @@ if img is None:
 def process_image(image):
     image = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
     image = cv.Canny(image, 100, 150)
-    image = cv.rotate(image, cv.ROTATE_180)
-    image = cv.flip(image, 1)
+    # image = cv.rotate(image, cv.ROTATE_180)
+    # image = cv.flip(image, 1)
 
     return image
 
@@ -115,13 +117,10 @@ def determine_concavity_vertical(left, circ):
     h, k = circ[0], circ[1]
 
     if y == k:
-        # print("0???")
         return 0
-    if -1/(y - k) + ((x - h) ** 2) / ((y - k) ** 3) > 0:
-        # print("concave up")
+    if -1 / (y - k) + ((x - h) ** 2) / ((y - k) ** 3) > 0:
         return 1
-    elif -1/(y - k) + ((x - h) ** 2) / ((y - k) ** 3) < 0:
-        # print("concave down")
+    elif -1 / (y - k) + ((x - h) ** 2) / ((y - k) ** 3) < 0:
         return -1
     else:
         print("huh?", left, circ)
@@ -132,32 +131,32 @@ def determine_concavity_horizontal(upper, circ):
     h, k = circ[0], circ[1]
 
     if x == h:
-        # print("0")
         return 0
-    if -1/(x - h) - ((y - k) ** 2) / ((x - h) ** 3) > 0:
-        # print("concave right")
+    if -1 / (x - h) - ((y - k) ** 2) / ((x - h) ** 3) > 0:
         return 1
-    elif -1/(x - h) - ((y - k) ** 2) / ((x - h) ** 3) < 0:
-        # print("concave left")
+    elif -1 / (x - h) - ((y - k) ** 2) / ((x - h) ** 3) < 0:
         return -1
     else:
         print("huh??", upper, circ)
 
 
-# determine_concavity_horizontal([2, 276], [51.5, 279.5, 2462.5])
+def calculate_contour_similarity(contour1, contour2):
+    return cv.matchShapes(contour1, contour2, cv.CONTOURS_MATCH_I1, 0.0)
 
 
-def remove_similar_contours(con):
-    contour1 = contours[0]
-    contour2 = contours[1]
+def remove_similar_contours(cons):
+    unique_contours = []
 
-    # Calculate Hu Moments
-    moments1 = cv.moments(contour1)
+    for i in cons:
+        unique = True
+        for j in unique_contours:
+            if calculate_contour_similarity(i, j) < 0.01 and len(i) > 30:
+                unique = False
+                break
+        if unique:
+            unique_contours.append(i)
 
-    moments2 = cv.moments(contour2)
-
-    # Calculate shape similarity using matchShapes
-    shape_similarity = cv.matchShapes(contour1, contour2, cv.CONTOURS_MATCH_I1, 0.0)
+    return unique_contours
 
 
 def get_contour_points(con, frequency):
@@ -166,10 +165,10 @@ def get_contour_points(con, frequency):
 
     print(len(con), "len contours")
 
-    for i in range(len(contours)):
+    for i in range(len(con)):
         for j in range(len(con[i])):
             if count == frequency:
-                print("(%d, %d)" % (contours[i][j][0][0], contours[i][j][0][1]))
+                # print("(%d, %d)" % (contours[i][j][0][0], contours[i][j][0][1]))
                 coords.append([con[i][j][0][0], con[i][j][0][1]])
                 count = 0
             else:
@@ -212,10 +211,23 @@ def shift_points(coord_set, shift_x, shift_y):
     return np.asarray(shifted_points)
 
 
-img = process_image(img)
-contours, hierarchy = cv.findContours(img, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+def bgr_to_hex(b, g, r):
+    return '#{:02x}{:02x}{:02x}'.format(r, g, b)
 
-display_image(img)
+
+def get_color(coord_set, image):
+    color = image[coord_set[1][1], coord_set[1][0]]
+
+    return bgr_to_hex(color[0], color[1], color[2])
+
+
+processed_img = process_image(img)
+contours, hierarchy = cv.findContours(processed_img, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+# contours = remove_similar_contours(contours)
+
+# print(remove_similar_contours(contours))
+
+display_image(processed_img)
 
 points = get_contour_points(contours, 6)
 # points = shift_points(points, 1.9, 4)
@@ -225,6 +237,8 @@ num_equations = 0
 for p in points:
     x_bounds = calculate_x_bounds(p)
     y_bounds = calculate_y_bounds(p)
+    clr = get_color(p, img)
+    print(clr)
     if not contains_duplicate(p):
         num_equations += 1
         if is_collinear(p):
@@ -242,28 +256,24 @@ for p in points:
                 horizontal_concavity = determine_concavity_horizontal([y_bounds[2][0], y_bounds[2][1]], circle)
                 if vertical_concavity == 1:
                     if horizontal_concavity == 1:
-                        print("(x - %0.2f)^2 + (y - %0.2f)^2 = %0.2f \\left\\{%0.2f - 0.7 < x < %0.2f\\right\\} \\left\\{%0.2f - 0.7 < y "
+                        print("(x - %0.2f)^2 + (y - %0.2f)^2 = %0.2f "
+                              "\\left\\{%0.2f - 0.7 < x < %0.2f\\right\\} \\left\\{%0.2f - 0.7 < y "
                               "< %0.2f\\right\\}"
                               % (circle[0], circle[1], circle[2], x_bounds[0], x_bounds[1], y_bounds[0], y_bounds[1]))
                     else:
-                        print("(x - %0.2f)^2 + (y - %0.2f)^2 = %0.2f \\left\\{%0.2f < x < %0.2f + 0.7\\right\\} \\left\\{%0.2f - 0.7 < y "
-                              "< %0.2f\\right\\}"
+                        print("(x - %0.2f)^2 + (y - %0.2f)^2 = %0.2f "
+                              "\\left\\{%0.2f < x < %0.2f + 0.7\\right\\} \\left\\{%0.2f - 0.7 < y < %0.2f\\right\\}"
                               % (circle[0], circle[1], circle[2], x_bounds[0], x_bounds[1], y_bounds[0], y_bounds[1]))
                 elif vertical_concavity == -1:
                     if horizontal_concavity == 1:
-                        print("(x - %0.2f)^2 + (y - %0.2f)^2 = %0.2f \\left\\{%0.2f - 0.7 < x < %0.2f\\right\\} \\left\\{%0.2f < y "
-                              "< %0.2f + 0.7\\right\\}"
+                        print("(x - %0.2f)^2 + (y - %0.2f)^2 = %0.2f "
+                              "\\left\\{%0.2f - 0.7 < x < %0.2f\\right\\} \\left\\{%0.2f < y < %0.2f + 0.7\\right\\}"
                               % (circle[0], circle[1], circle[2], x_bounds[0], x_bounds[1], y_bounds[0], y_bounds[1]))
                     else:
-                        print(
-                            "(x - %0.2f)^2 + (y - %0.2f)^2 = %0.2f \\left\\{%0.2f < x < %0.2f + 0.7\\right\\} \\left\\{%0.2f < y "
-                            "< %0.2f + 0.7\\right\\}"
-                            % (circle[0], circle[1], circle[2], x_bounds[0], x_bounds[1], y_bounds[0], y_bounds[1]))
-                # else:
-                #     print(
-                #         "(x - %0.2f)^2 + (y - %0.2f)^2 = %0.2f \\left\\{%0.2f < x < %0.2f\\right\\} \\left\\{%0.2f < y "
-                #         "< %0.2f\\right\\}"
-                #         % (circle[0], circle[1], circle[2], x_bounds[0], x_bounds[1], y_bounds[0], y_bounds[1]))
+                        print("(x - %0.2f)^2 + (y - %0.2f)^2 = %0.2f "
+                              "\\left\\{%0.2f < x < %0.2f + 0.7\\right\\} \\left\\{%0.2f < y < %0.2f + 0.7\\right\\}"
+                              % (circle[0], circle[1], circle[2], x_bounds[0], x_bounds[1], y_bounds[0], y_bounds[1]))
+
 
 print("Number of equations:", num_equations)
-
+print("coord_list length:", len(points))
