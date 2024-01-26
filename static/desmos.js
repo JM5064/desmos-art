3356a2ac83aa91699d83e5105c56c845e47fab7b
Justin <thisisjustinmao@gmail.com>
2024-01-26 06:54:17 -0500

    render desmos with color

--- static/desmos.js
+++ static/desmos.js
@@ -2,26 +2,16 @@ var elt = document.getElementById('calculator');
 var calculator = Desmos.GraphingCalculator(elt);
 
 lines = equations.split('\n');
-console.log(lines.length)
+
+var color = '#000000';
 
 for (var i = 0; i < lines.length; i++) {
-    calculator.setExpression({ id: i, color: null, latex: lines[i] });
-}
-
-
-state = calculator.getState();
-
-var color = "#000000";
-
-for (i=0;i<state.expressions.list.length;i++) {
-    var equation = state.expressions.list[i].latex;
-    if (equation !== undefined && equation.startsWith('#')) {
-        color = state.expressions.list[i].latex;
+    if (lines[i].startsWith('#')) {
+        color = lines[i];
+    } else {
+        calculator.setExpression({ id: i, color: color, latex: lines[i] });
     }
-    state.expressions.list[i].color=color;
 }
-
-calculator.setState(state);
 
 calculator.setMathBounds({
     left: -75.065,
