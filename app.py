9a7ede61d565f02b37d50a18425c48eebb20f6f5
Justin <thisisjustinmao@gmail.com>
2024-01-26 11:14:41 -0500

    upload and download multiple images

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template, request
 import requests
 from werkzeug.datastructures import FileStorage
 from io import BytesIO
+import json
 import main
 
 app = Flask(__name__, static_url_path='/static')
-result = ""
+result = []
 
 
 @app.route('/')
@@ -22,31 +23,59 @@ def secret():
 def desmos():
     try:
         global result
-        return render_template('desmos.html', equations=result)
+        return render_template('desmos.html', equations=json.dumps(result))
     except Exception as e:
         return str(e)
 
+
+@app.route('/', methods=['POST'])
+# def run_script():
+#     try:
+#         global result
+
+#         image_file = request.files['image']
+
+#         path = 'temp_path'
+#         image_file.save(path)
+
+#         image = main.get_image(path)
+
+#         if image is None:
+#             return render_template('index.html')
+
+#         equation_image = main.EquationImage(image)
+
+#         # equation_image.display_image()
+
+#         result = equation_image.get_equations(image)
+
+#         return render_template('index.html', equations=result)
+#     except Exception as e:
+#         return str(e)
+    
 
 @app.route('/', methods=['POST'])
 def run_script():
     try:
         global result
 
-        image_file = request.files['image']
+        image_file = request.files.getlist('image')
+        print(len(image_file))
 
-        path = 'temp_path'
-        image_file.save(path)
+        for image in image_file:
+            path = 'temp_path'
+            image.save(path)
 
-        image = main.get_image(path)
+            image = main.get_image(path)
 
-        if image is None:
-            return render_template('index.html')
+            if image is None:
+                return render_template('index.html')
 
-        equation_image = main.EquationImage(image)
+            equation_image = main.EquationImage(image)
 
-        # equation_image.display_image()
+            # equation_image.display_image()
 
-        result = equation_image.get_equations(image)
+            result.append(equation_image.get_equations(image))
 
         return render_template('index.html', equations=result)
     except Exception as e:
--- static/desmos.js
+++ static/desmos.js
@@ -1,21 +1,107 @@
 var elt = document.getElementById('calculator');
 var calculator = Desmos.GraphingCalculator(elt);
 
-lines = equations.split('\n');
 
-var color = '#000000';
-
-for (var i = 0; i < lines.length; i++) {
-    if (lines[i].startsWith('#')) {
-        color = lines[i];
-    } else {
-        calculator.setExpression({ id: i, color: color, latex: lines[i] });
-    }
+function setBounds() {
+    calculator.setMathBounds({
+        left: -75.065,
+        right: 1405.354,
+        bottom: -161.045,
+        top: 934.883
+      });
 }
 
-calculator.setMathBounds({
-    left: -75.065,
-    right: 1405.354,
-    bottom: -161.045,
-    top: 934.883
-  });
+setBounds();
+
+
+async function drawLines(lines) {
+    return new Promise(async resolve => {
+        console.log("hello");
+
+        // lines = equations[img];
+    
+        var color = '#000000';
+    
+        for (var i = 0; i < lines.length; i++) {
+            if (lines[i].startsWith('#')) {
+                color = lines[i];
+            } else {
+                calculator.setExpression({ id: i, color: color, latex: lines[i] });
+            }
+        }
+    
+        await takeScreenshot();
+        resolve();
+    });
+    
+}
+
+
+async function takeScreenshot() {
+    return new Promise(async resolve => {
+        var opts = {
+            "mode": "contain",
+            "width": window.screen.width,
+            "height": window.screen.height,
+            "targetPixelRatio": 1,
+            "preserveAxisNumers": false
+        };
+    
+        var data = await calculator.asyncScreenshot(opts, downloadImage);
+        await removeLines();
+        resolve();
+    })
+
+    
+}
+
+
+async function downloadImage(imageData) {
+    return new Promise(async resolve => {
+        var img = document.createElement('a');
+        img.style.display = 'none';
+        
+        img.href = imageData;
+    
+        img.download = 'image.jpg';
+    
+        document.body.appendChild(img);
+    
+        img.click();
+    
+        document.body.removeChild(img);
+    
+        // await removeLines();
+        resolve();
+    })
+    
+}
+
+async function removeLines() {
+    return new Promise(async resolve => {
+        var expressions = calculator.getExpressions();
+        for (var i = 0; i < expressions.length; i++) {
+            calculator.removeExpression({ id: expressions[i].id})
+        }
+        resolve();
+    })
+}
+
+
+async function processEquations() {
+    for (var img = 0; img < equations.length; img++) {
+        await drawLines(equations[img]);
+    }
+    
+    // for await (lines of equations) {
+    //     await drawLines(lines);
+    // }
+    console.log("are you waiting?")
+}
+
+processEquations();
+
+
+
+
+
