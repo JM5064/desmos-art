2f5616ca68ec23b46fc9bb11f0ce128eaa3e344b
Justin <thisisjustinmao@gmail.com>
2024-03-06 20:19:20 -0500

    bezier bisector

---
+++ bezier2.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def calculate_bisector_control_point(p1, p2, p3):
+    # check for collinear
+
+    hyp = np.subtract(np.array(p3), np.array(p1))   # c
+    p12 = np.subtract(np.array(p2), np.array(p1))   # a
+    print(hyp, p12)
+    midpoint = np.add(p1, p2) * 0.5
+    print(midpoint)
+
+    A = np.asarray([ [hyp[1], -hyp[0]],  [p12[0], p12[1]] ])
+    b = np.asarray([ [p2[0] * hyp[1] - p2[1] * hyp[0]],   [p12[0] * midpoint[0] + p12[1] * midpoint[1]] ])
+    
+    try:
+        x, y = np.linalg.solve(A, b)
+        return [x, y]
+    
+    except np.linalg.LinAlgError:
+        print("Inconsistent system", A, b)
+        return None
+
+# print(calculate_bisector_control_point([1,0.5], [2,1], [4,1]))
+print(calculate_bisector_control_point([6,0], [4,1], [2,1]))
