37cb420780d5553c7ad770fcd63d3e13e043601f
Justin <thisisjustinmao@gmail.com>
2024-04-24 23:13:09 -0400

    getcontours, bezier equations

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import utils
 import sys
 import io
+import bezier
 
 
 def get_image(path):
@@ -15,7 +16,6 @@ def get_image(path):
         img = cv.rotate(img, cv.ROTATE_180)
         img = cv.flip(img, 1)
 
-        
         return img
 
 
@@ -30,23 +30,30 @@ class EquationImage:
 
 
     def process_image(self):
+        # self.image = cv.blur(self.image, (2,3))
         self.image = cv.blur(self.image, (3,3))
+        # self.image = cv.blur(self.image, (4,4))
+        # self.image = cv.blur(self.image, (5,5))
+        # self.image = cv.blur(self.image, (6,6))
         self.image = cv.cvtColor(self.image, cv.COLOR_RGB2GRAY)
-        self.image = cv.Canny(self.image, 20, 30)
+        self.image = cv.Canny(self.image, 30, 100)
 
 
     def display_image(self):
         cv.imshow("Image", self.image)
         cv.waitKey(0)
 
-
-    def calculate_points(self):
+    
+    def get_contours(self):
         self.process_image()
+
         contours, hierarchy = cv.findContours(self.image, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
         # contours = utils.remove_similar_contours(contours)
 
-        # self.display_image()
+        return contours
 
+
+    def calculate_circle_points(self, contours):        
         points = utils.get_threes(contours, 10)
 
         points = utils.shift_points(points, self.shift_x, self.shift_y)
@@ -58,24 +65,38 @@ class EquationImage:
     def get_color(self, coord_set, image):
         color = np.array(image[coord_set[1][1], coord_set[1][0]], np.int16)
 
-        # if abs(color[0] - color[1]) < 30 and abs(color[0] - color[2]) < 30:
-        #     color[0] += 40
-        #     if color[0] > 255:
-        #         color[0] = 255
-        #     color[1] += 40
-        #     if color[1] > 255:
-        #         color[1] = 255
-        #     color[2] += 40
-        #     if color[2] > 255:
-        #         color[2] = 255
-
         return utils.bgr_to_hex(color[0], color[1], color[2])
+    
+
+    def calculate_bezier_points(self, contours):
+        points = utils.reduce_bezier_contour_points(contours, 2)
+
+        # points = utils.shift_points(points, self.shift_x, self.shift_y)
+        # points = utils.scale_points(points, self.scale_factor)
+
+        return points
+    
+
+    def print_bezier_equations(self):
+        contours = self.get_contours
+        
+        reduced_contours = self.calculate_bezier_points(contours)
+
+        for contour in reduced_contours:
+            # print("contour test: ", contour)
+            # print("first point:", contour[0][0])
+            curve = bezier.bezier(contour)
+
+            curve.get_curve_equations()
+            # break
 
 
     def get_circle_equations(self, image):
+        # self.print_bezier_equations()
         equations = []
 
-        threes = self.calculate_points()
+        contours = self.get_contours()
+        threes = self.calculate_circle_points(contours)
 
         for points in threes:
             x_bounds = utils.calculate_x_bounds(points)
